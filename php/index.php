5afe710005a914921673759e1eb6711ab2fdb5fa
Nitish sahni <nitish@realmeta.ca>
2025-04-29 17:25:43 -0300

    change2

--- php/index.php
+++ php/index.php
@@ -2,6 +2,6 @@
    
    $heading = 'Home Page is This One.';
 
-   
+   var_dump('$_SERVER');
 
 require "views/index.view.php";
--- php/views/contact.view.php
+++ php/views/contact.view.php
@@ -1,14 +1,19 @@
-<?php require('views/particals/head.php') ?>
-<?php require('views/particals/nav.php') ?>
-<?php require('views/particals/banner.php')?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$heading = "Contact Us"; // Define the heading variable
+require('views/particals/head.php');
+require('views/particals/nav.php');
+require('views/particals/banner.php');
+?>
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <P>Hello. This is our Contact us page.</P>
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
+    <p>Hello. This is our Contact us page.</p>
   </div>
 </main>
 </div>
-
-
 </body>
-
 </html>
