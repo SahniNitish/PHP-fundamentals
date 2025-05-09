ef2c1a09ca84b6dae74b0a59e7d542d85754ba07
Nitish sahni <nitish@realmeta.ca>
2025-05-09 12:44:10 -0300

    Making-Routes

---
+++ php/controllers/index.php
@@ -0,0 +1,7 @@
+<?php 
+   
+   $heading = 'Home Page is This One.';
+
+   var_dump('$_SERVER');
+
+require "views/index.view.php";
--- php/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-   
-   $heading = 'Home Page is This One.';
-
-   var_dump('$_SERVER');
-
-require "views/index.view.php";
