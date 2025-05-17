c5e2d5b25793c707519bfca3b28002637513b5c7
Nitish sahni <nitish@realmeta.ca>
2025-05-17 05:32:33 -0300

    Update footer.php

---
+++ php/views/particals/footer.php
@@ -0,0 +1,18 @@
+<?php require('views/particals/head.php') ?>
+<?php require('views/particals/nav.php') ?>
+<?php require('views/particals/banner.php') ?>
+
+
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <P>Hello. Now you are on the About page.</P>
+  </div>
+</main>
+<?php require('views/particals/footer.php') ?>
+
+</div>
+
+
+</body>
+
+</html>
